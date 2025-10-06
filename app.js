deb3df961bc3e7d89fdcb9beae9b2abcec9717d3
GeethikaReddy2306 <geethikareddy051@gmail.com>
2025-10-06 21:26:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+function  getElement(input){
+        display.value+=input;
+}
+function clearDisplay(){
+        display.value="";
+}
+function  calculate() {
+try{
+        display.value=eval(display.value);
+}catch(error){
+        display.value="Error";
+}
+}
